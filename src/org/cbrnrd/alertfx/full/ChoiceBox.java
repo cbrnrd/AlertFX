d196648653e9e8902ff90419ef1a8139d9406807
cbrnrd <cbawsome77@gmail.com>
2017-05-29 22:22:01 -0400

    Finish ChoiceBox

--- src/org/cbrnrd/alertfx/full/ChoiceBox.java
+++ src/org/cbrnrd/alertfx/full/ChoiceBox.java
@@ -5,11 +5,13 @@ import javafx.scene.control.ChoiceDialog;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 
 public class ChoiceBox {
 
-    List<String> choices = new ArrayList<>();
+    private List<String> choices = new ArrayList<>();
+    private ChoiceDialog<String> alert;
 
     public ChoiceBox(String... choices){
         Collections.addAll(this.choices, choices);
@@ -20,8 +22,15 @@ public class ChoiceBox {
         choices.add(choice);
     }
 
-    public void show(){
-        ChoiceDialog<String> alert = new ChoiceDialog<>(null, choices);
+    public void setHeader(String header){
+        alert.setHeaderText(header);
+    }
+
+
+    public String show(){
+        alert = new ChoiceDialog<>(null, choices);
+        Optional<String> result = alert.showAndWait();
+        return result.orElse(null);  // God bless intellij help
 
     }
 
