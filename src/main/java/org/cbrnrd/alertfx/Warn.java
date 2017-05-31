763dff84e764a812967335f82f058e7dd821afb8
Carter Brainerd (thecarterb) <cbawsome77@gmail.com>
2017-05-31 10:57:56 -0400

    Add javadoc for warn
    TODO generate it

--- src/main/java/org/cbrnrd/alertfx/Warn.java
+++ src/main/java/org/cbrnrd/alertfx/Warn.java
@@ -5,7 +5,7 @@ import javafx.scene.control.DialogPane;
 import javafx.stage.StageStyle;
 
 /**
- * TODO javadoc
+ * Shows a warning alert
  */
 public class Warn {
 
@@ -23,10 +23,19 @@ public class Warn {
         this.warning = warning;
     }
 
+    /**
+    * Changes the header of the alert
+    * @param head The header to show
+    */
     public void setHeader(String head){
         alert.setHeaderText(head);
     }
 
+    /**
+    * Changes the size of the alert
+    * @param x The size on the x-axis
+    * @param y The size on the y-axis
+    */
     public void setSize(double x, double y){
         alert.setX(x);
         alert.setY(y);
@@ -42,6 +51,9 @@ public class Warn {
         dialogPane.getStyleClass().add(path.replace(".css", ""));  // Shouldn't have extension
     }
 
+    /**
+    * Shows the alert
+    */
     public void show(){
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.initStyle(style);
@@ -50,6 +62,9 @@ public class Warn {
         alert.showAndWait();
     }
 
+    /**
+    * Shows the alert but isn't always on top
+    */
     public void showNonblock(){
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.initStyle(style);
